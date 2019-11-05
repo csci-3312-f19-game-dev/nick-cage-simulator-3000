8218509f5b7f9b32363ed5f58f0b46f1bde5a9b9
kghent <kghent@trinity.edu>
2019-11-05 16:44:23 -0600

    game manager with timer

---
+++ nickCageSimulator3000/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float startTime;
+    int day;
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        startTime = Time.time;
+        day = 1;
+    }
+
+    void Update()
+    {
+        if((Time.time - startTime) % 100 > day)
+        {
+            day++;
+        }
+    }
+}

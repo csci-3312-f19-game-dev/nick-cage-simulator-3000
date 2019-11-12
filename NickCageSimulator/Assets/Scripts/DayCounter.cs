c023e3ec48de9da02ab4af921d65f2d77a0cfe12
kghent <kghent@trinity.edu>
2019-11-11 19:50:47 -0600

    map downsized, day counter and basic ui canvas

---
+++ NickCageSimulator/Assets/Scripts/DayCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System.Threading;
+
+public class DayCounter : MonoBehaviour
+{
+    private int day;
+    public Text dayDisplay;//assigned in inspector, so must be public
+
+    void Start()
+    {
+        day = 0;
+        Time.timeScale = 1;
+        StartCoroutine("UpdateDay");
+    }
+
+    void Update()
+    {
+        dayDisplay.text = ("Days Gone By: " + day);
+    }
+
+    //Will run independent of everything else... I think?
+    IEnumerator UpdateDay()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5);
+            day++;
+            //Debug.Log("Just added a day");
+        }
+    }
+}

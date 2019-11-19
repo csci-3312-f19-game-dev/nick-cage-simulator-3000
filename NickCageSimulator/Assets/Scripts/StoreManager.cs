7835bd81038dfb49c9caf9924be5749af1163629
Maddie Cox <mcox1@trinity.edu>
2019-11-19 09:46:27 -0600

    Implement Shop Menu Toggle

---
+++ NickCageSimulator/Assets/Scripts/StoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour
+{
+    public GameObject storeScreen;
+    public GameObject brokenPurchaseButton;
+    private bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        storeScreen.GetComponent<Renderer>().enabled = false;
+        //brokenPurchaseButton.GetComponent<Renderer>().enabled = false;
+        isOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        storeScreen.GetComponent<Renderer>().enabled = !isOpen;
+        //brokenPurchaseButton.GetComponent<Renderer>().enabled = !isOpen;
+        isOpen = !isOpen;
+    }
+}

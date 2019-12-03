51af285940fef2888a39eea4a99cd94bf4c07777
Maddie Cox <mcox1@trinity.edu>
2019-12-03 13:48:31 -0600

    Added Start Screen

--- NickCageSimulator/Assets/Scripts/StorePurchaseHandler.cs
+++ NickCageSimulator/Assets/Scripts/StorePurchaseHandler.cs
@@ -7,7 +7,6 @@ public class StorePurchaseHandler : MonoBehaviour
     public Resource purchasing;
 
     public void OnMouseDown(){
-        Debug.Log("buying" + purchasing + " with " + currency);
         PlayerManager.PM.makePurchase(purchasing, currency);
     }
 }
